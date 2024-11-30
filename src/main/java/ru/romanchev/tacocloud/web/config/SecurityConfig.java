5b3ae7e069558cb56be63b6961e3f9b0510799f9
VillRom <alexromanchev@mail.ru>
2024-12-01 00:25:52 +0400

    init SSecurity

---
+++ src/main/java/ru/romanchev/tacocloud/web/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package ru.romanchev.tacocloud.web.config;
+
+import org.apache.logging.log4j.message.SimpleMessage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        List<UserDetails> userDetails = new ArrayList<>();
+        userDetails.add(new User("buzz", encoder.encode("password"),
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+        userDetails.add(new User("woody", encoder.encode("password"),
+                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
